f6863c7e6fb869416680a2f87f8a1c68fc63424f
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-03-29 18:13:45 +0530

    fixed rate producer

--- src/main/java/com/course/kafkaproducer/KafkaProducerApplication.java
+++ src/main/java/com/course/kafkaproducer/KafkaProducerApplication.java
@@ -1,24 +1,16 @@
 package com.course.kafkaproducer;
 
-import com.course.kafkaproducer.producer.HelloKafkaProducer;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @AllArgsConstructor
-public class KafkaProducerApplication implements CommandLineRunner
+public class KafkaProducerApplication
 {
-	private final HelloKafkaProducer kafkaProducer;
-
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaProducerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception
-	{
-		kafkaProducer.produce("Hello, sample kafka producer msg - 1");
 	}
 }
---
+++ src/main/java/com/course/kafkaproducer/producer/FixedRateProducer.java
@@ -0,0 +1,24 @@
+package com.course.kafkaproducer.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class FixedRateProducer
+{
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    private int i = 0;
+
+    @Scheduled(fixedRate = 1000)
+    public void produce()
+    {
+        log.info("Value of i being sent: {}", i);
+        kafkaTemplate.send("t_fixedrate", "Message no. " + i++);
+    }
+}
